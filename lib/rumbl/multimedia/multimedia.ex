b1f31076e26161bb00effee01eeed778ecdb0f4d
Richard Alam <ritzalam@gmail.com>
2018-10-01 19:19:57 +0000

    - ch7 constraints

--- lib/rumbl/multimedia/multimedia.ex
+++ lib/rumbl/multimedia/multimedia.ex
@@ -8,6 +8,17 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Accounts
 
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
+
+  def create_category(name) do
+    Repo.get_by(Category, name: name) || Repo.insert!(%Category{name: name})
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
 
   @doc """
   Returns the list of videos.
