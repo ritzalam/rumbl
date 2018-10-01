321afb8770b4267b0543edbcfd71aef4b1a6316a
Richard Alam <ritzalam@gmail.com>
2018-10-01 16:19:12 +0000

    - ch6 generators and relationships

--- lib/rumbl/multimedia/multimedia.ex
+++ lib/rumbl/multimedia/multimedia.ex
@@ -19,7 +19,27 @@ defmodule Rumbl.Multimedia do
 
   """
   def list_videos do
-    Repo.all(Video)
+    Video
+    |> Repo.all()
+    |> preload_user()
+  end
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+    |> preload_user()
+  end
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    from(v in Video, where: v.id == ^id)
+    |> user_videos_query(user)
+    |> Repo.one!()
+    |> preload_user()
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
   end
 
   @doc """
@@ -36,7 +56,11 @@ defmodule Rumbl.Multimedia do
       ** (Ecto.NoResultsError)
 
   """
-  def get_video!(id), do: Repo.get!(Video, id)
+  def get_video!(id), do: preload_user(Repo.get!(Video, id))
+
+  defp preload_user(video_or_videos) do
+    Repo.preload(video_or_videos, :user)
+  end
 
   @doc """
   Creates a video.
