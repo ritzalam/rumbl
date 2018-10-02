c5e94ea84058dfa39b491c9e04f06654204bc056
Richard Alam <ritzalam@gmail.com>
2018-10-02 19:09:52 +0000

    - ch10 channels

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -9,15 +9,32 @@ defmodule RumblWeb.Auth do
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)
-    user = user_id && Accounts.get_user(user_id)
-    assign(conn, :current_user, user)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+
+      user = user_id && Accounts.get_user(user_id) ->
+        put_current_user(conn, user)
+
+      true ->
+        assign(conn, :current_user, nil)
+    end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def login_by_email_and_pass(conn, email, given_pass) do
